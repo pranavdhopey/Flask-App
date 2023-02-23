504884cd0174d6cc37e32105b61a47f9fa3586b2
pranavdhopey <pranav.dhope@gmail.com>
2023-02-23 14:21:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello App!"
+    return "Hello Jenkins!"
 # run the app.
 if __name__ == "__main__":
     app.run()
